c33a6d090f69a86d546288a3a997a1917adea761
LoseControll942 <b.chester90@yahoo.com>
2026-02-18 11:52:53 +0000

    Added main menu, pause menu, and HUD

--- Assets/Player/Scripts/HealthManager.cs
+++ Assets/Player/Scripts/HealthManager.cs
@@ -1,29 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthManager : MonoBehaviour
 {
-    [SerializeField]
-    float hitPoints = 100f;
+    [SerializeField] float maxHitPoints = 100f;
+    float hitPoints;
+
+    public Slider healthSlider;
+
+    void Start()
+    {
+        hitPoints = maxHitPoints;
+        SetHealthSlider();
+    }
+
     void Hit(float rawDamage)
-    { 
+    {
         hitPoints -= rawDamage;
+        SetHealthSlider();
 
         Debug.Log("OUCH: " + hitPoints.ToString());
 
         if (hitPoints <= 0)
         {
             Debug.Log("TODO: GAME OVER - YOU DIED");
-            Destroy(gameObject);
         }
     }
-
     void Heal(float HealthUp)
     {
         hitPoints += HealthUp;
+        SetHealthSlider();
 
         Debug.Log("Healed: " + hitPoints.ToString());
 
+    }
+
+    void SetHealthSlider()
+    {
+        if (healthSlider != null)
+        {
+            healthSlider.value = NormalisedHitPoints();
+        }
+    }
+
+    float NormalisedHitPoints()
+    {
+        return hitPoints / maxHitPoints;
     }
 }
